33f99cb2062b84a309a386c55efb0a06709013e4
eddemo0007 <eddemo0007@gmail.com>
2024-04-08 13:33:53 -0400

    Added New files

---
+++ Python0001.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    num = int(input("Enter a number: "))
+    result = factorial(num)
+    print("The factorial of", num, "is", result)
+
+if __name__ == "__main__":
+    main()
